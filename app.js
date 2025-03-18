106f66282f08b9080c199fc9b5cd1771d5ec3771
Yabilá May <cris.yabimaychi@gmail.com>
2025-03-17 19:08:27 -0600

    crea el array y funciones

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//array amigos
+let amigos=[];
+
+//función agregar amigos
+function agregarAmigo() {
+    
+}
+//función actualizar la lista de amigos
+function actualizarLista() {
+    
+}
+//función para sortear los amigos
+function sortearAmigo() {
+    
+}
